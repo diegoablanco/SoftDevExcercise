16d2c69e6025bd17774633b3a6b4f9698a140d76
diego <diegoablanco@gmail.com>
2018-08-04 18:48:20 -0300

    kein test

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
+require '../vendor/autoload.php';
 $config = require('../config/config.php');
+$klein = new \Klein\Klein();
+
 echo($config['host']);
+echo($_GET['url']);
+
+$klein->respond('[**:name]', function ($request) {
+    // var_dump($request);
+    echo('Route is ' . $request->name);
+});
+$klein->dispatch();
 ?>
---
+++ src/controllers/rankingController.php
@@ -0,0 +1,3 @@
+<?php
+echo('ranking controller');
+?>
