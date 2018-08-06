22407eb84d7b30a3af1eb12dfb03feae512f1570
diego <diegoablanco@gmail.com>
2018-08-05 23:26:54 -0300

    google ranking added

--- src/controllers/rankingController.php
+++ src/controllers/rankingController.php
@@ -3,7 +3,24 @@ class rankingController
 {
     function get()
     {
-        echo json_encode(['ranking controller']);
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', 'https://www.googleapis.com/books/v1/volumes', [
+            'query' => [
+                'q' => 'inauthor:Tolkien',
+                'maxresults' => '10',
+                'orderBy' => 'newest',
+                'filter' => 'ebooks'
+                ]
+        ]);
+        $body = json_decode($res->getBody(), true);
+        $books = array_map(function($book){
+            $volumeInfo = $book['volumeInfo'];
+            return [
+                'title' => $volumeInfo['title'],
+                'previewLink' => $volumeInfo['previewLink']                
+            ]; }, 
+            $body['items']);
+        echo(json_encode($books));
     }
 }
 ?>
--- src/views/index.php
+++ src/views/index.php
@@ -4,57 +4,106 @@
 	<title>Software development test</title>
 	<meta charset="utf-8">
  
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">	
 </head>
 <body>
+    <div class="container-fluid">
 	<header>
 		<h1>Top 100 Tolkien's books</h1>
 	</header>
-    <section>
-        <div class="shadow-sm p-3 mb-5 bg-white rounded">
-        <form  method="POST">
-            <div class="form-group">
-                <label for="searchInput">Find by title</label>
-                <input 
-                    type="text" 
-                    name="search" 
-                    id="searchInput" 
-                    class="form-control" 
-                    aria-describedby="enter title" 
-                    placeholder="Enter title here"
-                    value="<?=$form['search'] ?>">
+    <section>  
+        <div class="row">
+            <div class="col-sm">
+                <div class="shadow-sm p-3 mb-5 bg-white rounded">
+                    <form  method="POST">
+                        <div class="form-group">
+                            <label for="searchInput">Find by title</label>
+                            <input 
+                                type="text" 
+                                name="search" 
+                                id="searchInput" 
+                                class="form-control" 
+                                aria-describedby="enter title" 
+                                placeholder="Enter title here"
+                                value="<?=$form['search'] ?>">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Find</button>
+                    </form>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary">Find</button>
-        </form>
+            <div class="col-lg" />
         </div>
     </section>
 	<section>
-        <div class="container-fluid">
-            <table class="table">
-                <thead>
-                    <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Author</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">First Publish Year</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($model as $book):?>
+        <div class="row">
+            <div class="col-9">
+                <div class="shadow-sm p-3 mb-5 bg-white rounded">
+                <table class="table">
+                    <thead>
                         <tr>
-                        <th scope="row"><?= $book->id;?></th>
-                        <td><?= $book->author;?></td>
-                        <td><?= $book->title;?></td>
-                        <td><?= $book->publish_year;?></td>
+                        <th scope="col">ID</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">First Publish Year</th>
                         </tr>
-                    <?php endforeach;?>
-                </tbody>
-            </table>
+                    </thead>
+                        <tbody>
+                            <?php foreach($model as $book):?>
+                                <tr>
+                                <th scope="row"><?= $book->id;?></th>
+                                <td><?= $book->author;?></td>
+                                <td><?= $book->title;?></td>
+                                <td><?= $book->publish_year;?></td>
+                                </tr>
+                            <?php endforeach;?>
+                        </tbody>
+                    </table>
+                </div>
+	    </div>
+        <div class="col">
+            <div class="shadow-sm p-3 mb-5 bg-white rounded">
+                <table class="table" id="ranking">
+                    <thead>
+                        <tr>
+                            <th scope="col">Latest ebooks from Google Books</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Loading...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-	</section>
- 
+        
+        
+        </section>
+    </div>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $.ajax({
+                url: 'rankings', 
+                dataType: 'json',
+                success: function(data){
+                    var tableBody = $('#ranking tbody');
+                    tableBody.empty();
+                    data.forEach(function(book){
+                        var anchor = $('<a>')
+                            .attr('target', '_blank')
+                            .attr('href', book.previewLink)
+                            .text(book.title);
+                        var td = $('<td>').append(anchor);
+                        tableBody.append($('<tr>').append(td));
+                    });
+                    $('#ranking table')
+                    console.log(data);
+                }
+            })
+        })
+    </script>
 </body>
 </html>
